a8545a7250259e2e785ad976e50c0299c50278c6
Julian Vallario <vallario.julian@gmail.com>
2020-11-10 21:01:54 -0300

    se hizo el maquetado y estilos de Paso

---
+++ js/paso.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['paso'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"paso\">\n    <header>\n        <div class=\"number\">\n            01\n        </div>\n        <div class=\"text\">\n            <span class=\"titulo\">Encuentra el problema</span>\n            <p>¿Qué problema se resuelve primero? No importa.\n                Con tiempo y energía, se arregla el que más repercusión tenga\n                en el balance final.</p>\n        </div>\n    </header>\n    <div class=\"lista\">\n        <div>Marketing Research</div>\n        <div>Escucha a tu Equipo</div>\n        <div>Analizar el Problema</div>\n    </div>\n</div>";
+},"useData":true});
+})();
