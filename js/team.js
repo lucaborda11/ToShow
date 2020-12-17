206a3bef384262358e13a002a18e5eb115cb3960
Luca Borda <luca@northic.io>
2020-12-17 18:40:50 -0300

    cambios de textos y banderas agregadas.

--- js/team.js
+++ js/team.js
@@ -1,1 +1,17 @@
-!function(){var t=Handlebars.template;(Handlebars.templates=Handlebars.templates||{}).team=t({compiler:[8,">= 4.3.0"],main:function(t,e,n,a,l){var i,o=null!=e?e:t.nullContext||{},r=t.hooks.helperMissing,m="function",c=t.lookupProperty||function(t,e){if(Object.prototype.hasOwnProperty.call(t,e))return t[e]};return'      <div class="teamText">\n        <article>\n            '+(null!=(t=typeof(i=null!=(i=c(n,"team_text_1")||(null!=e?c(e,"team_text_1"):e))?i:r)==m?i.call(o,{name:"team_text_1",hash:{},data:l,loc:{start:{line:3,column:12},end:{line:3,column:29}}}):i)?t:"")+'\n        </article>\n        <img src="img/teamImg1.png" alt="">\n        <p>ALDP Program Boston Scientific.</p>\n        <br>\n        <br>\n        <article>\n            '+(null!=(t=typeof(i=null!=(i=c(n,"team_text_2")||(null!=e?c(e,"team_text_2"):e))?i:r)==m?i.call(o,{name:"team_text_2",hash:{},data:l,loc:{start:{line:10,column:12},end:{line:10,column:29}}}):i)?t:"")+'\n        </article>\n        <img src="img/teamImg.png" alt="">\n        <p>Mike Mahoney | CEO de Boston Scientific</p>\n        <p>Ortíz | Presidente Latam de Boston Scientific</p>\n      </div>  '},useData:!0})}();
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['team'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "      <div class=\"teamText\">\n        <article>\n            "
+    + ((stack1 = ((helper = (helper = lookupProperty(helpers,"team_text_1") || (depth0 != null ? lookupProperty(depth0,"team_text_1") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"team_text_1","hash":{},"data":data,"loc":{"start":{"line":3,"column":12},"end":{"line":3,"column":29}}}) : helper))) != null ? stack1 : "")
+    + "\n        </article>\n        <br>\n        <br>\n        <p>ALDP Program Boston Scientific.</p>\n        <p>Executive Commitee Boston Scientific, 2014</p>\n        <img src=\"img/teamImg1.png\" alt=\"\">\n        <br>\n        <br>\n        <article>\n            "
+    + ((stack1 = ((helper = (helper = lookupProperty(helpers,"team_text_2") || (depth0 != null ? lookupProperty(depth0,"team_text_2") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"team_text_2","hash":{},"data":data,"loc":{"start":{"line":13,"column":12},"end":{"line":13,"column":29}}}) : helper))) != null ? stack1 : "")
+    + "\n        </article>\n        <br>\n        <br>\n        <p>Mike Mahoney | CEO de Boston Scientific</p>\n        <p>Ortíz | Presidente Latam de Boston Scientific</p>\n        <img src=\"img/teamImg.png\" alt=\"\">\n      </div>  ";
+},"useData":true});
+})();
